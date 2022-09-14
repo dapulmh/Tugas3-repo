2e7047236071342bb4792664904559d6c0508fad
Daffa Maulana Haekal <maulanadaffa342@gmail.com>
2022-09-14 17:43:32 +0700

    my tugas 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_wishlist
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_wishlist, name='show_wishlist'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_wishlist(request):
+    data_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_item,
+        'nama': 'Daffa Maulana Haekal',
+        'id': '2106652083'
+    }
+    return render(request, "katalog.html", context)
